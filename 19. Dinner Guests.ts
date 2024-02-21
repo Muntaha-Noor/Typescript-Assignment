3218278b60d1259b284411e1ba310e4e98b17cf4
Muntaha-Noor <160227252+Muntaha-Noor@users.noreply.github.com>
2024-02-21 21:42:57 +0500

    Add files via upload

---
+++ 19. Dinner Guests.ts
@@ -0,0 +1,6 @@
+
+const guestList: string[] = ["Muntaha Noor", "Warda Noor", "Musfirah Noor"];
+
+const numberOfGuest: number = guestList.length;
+
+console.log(`You are inviting ${numberOfGuest} people to dinner.`);
