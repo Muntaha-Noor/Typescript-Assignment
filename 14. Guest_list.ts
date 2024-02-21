610a592c1045774377d259ebf27e94408f64752a
Muntaha-Noor <160227252+Muntaha-Noor@users.noreply.github.com>
2024-02-21 21:33:30 +0500

    Add files via upload

---
+++ 14. Guest_list.ts
@@ -0,0 +1,7 @@
+
+let Guest_List :string[] = ['Muntaha Noor', 'Warda Noor', 'Musfirah Noor'];
+
+for(let i=0; i<Guest_List.length; i++){
+     
+    console.log('Dear Miss.' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!\n\n')
+}
