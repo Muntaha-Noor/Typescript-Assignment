1d0865f0bf76b30118b303ae90c159a5c22d5902
Muntaha-Noor <160227252+Muntaha-Noor@users.noreply.github.com>
2024-02-21 22:22:54 +0500

    Add files via upload

---
+++ 41. Magicians.ts
@@ -0,0 +1,10 @@
+
+function show_magicians(magicians: string[]): void {
+    for(const magician of magicians) {
+        console.log(magician);
+    }
+}
+
+const magicianNames: string[] = ["Merlin", "David copperfield", "Houdini", "Luna"];
+
+show_magicians(magicianNames);
