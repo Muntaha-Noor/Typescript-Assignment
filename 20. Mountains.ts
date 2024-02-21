b9e5ac70d5e349e4d5eba5790cd1a572e285a22d
Muntaha-Noor <160227252+Muntaha-Noor@users.noreply.github.com>
2024-02-21 21:44:26 +0500

    Add files via upload

---
+++ 20. Mountains.ts
@@ -0,0 +1,10 @@
+
+const mountains : string[] = ["Everest", "K2", "Denali", "Matterhorn", "Kangchenjunga",]
+
+//Print the list of mountains
+console.log("List of mountains:");
+
+for(const mountain of mountains){
+    console.log(mountain);
+
+}
